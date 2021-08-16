5c0c21f82fa6fc7680749068c602cba6d8a96b96
adagun <adagun@hotmail.com>
2021-08-16 21:12:37 +0200

    env settings

--- NotesApp/settings.py
+++ NotesApp/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-s4f#p)!8rs&unif#3$#nm=0gw057dw)fj_#@)%gdp4sk*=^^+#'
+SECRET_KEY = os.getenv('SECRET_KEY', '=&$rh@!-&%)0(u86u#dxxq!&9=ei8y127tdsp&k8zeu7sr6ubl'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
