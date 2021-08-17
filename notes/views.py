b5adf7b0e40c070d531293dd5cb6cfd919e42779
adagun <adagun@hotmail.com>
2021-08-17 10:16:10 +0200

    brand link

--- notes/views.py
+++ notes/views.py
@@ -10,7 +10,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from .forms import NoteForm
-from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
